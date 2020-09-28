7b0d3a6336fd28ffa3d0cb12f8cf0c98cc222952
Cameron Bisca <cjbisca@gmail.com>
2020-09-28 21:27:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def input_to_index(user_input)
-  user_input = '5'.to_i
+  user_input = '-1'.to_i
 end
 
 # code your input_to_index and move method here!
