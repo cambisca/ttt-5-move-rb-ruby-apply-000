fe7b36320111ad2225bc0484989913ec45299b41
Cameron Bisca <cjbisca@gmail.com>
2020-09-28 21:15:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-user_input
+def input_to_index(user_input)
+end
 
 # code your input_to_index and move method here!
