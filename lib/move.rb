2def831a34c3f5ccfcf822f43b9bb9bf59223e7e
Cameron Bisca <cjbisca@gmail.com>
2020-09-28 21:38:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,9 +15,10 @@ end
 
 board = [" ", " ", " "]
 
-def update_array_at_with(array, index, value)
+def move(array, index, value)
   array[index] = value 
 end
+
 
 
 # code your input_to_index and move method here!
